b1c6a9c8fe249faa8b491cd47838644eaf236936
Aryan Sahay <asryan2004@gmail.com>
2024-06-07 10:36:32 +0530

    Added project files

---
+++ emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    head = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json = json, headers=head)
+
+    if response.status_code == 200:
+        return formated_response
+    elif response.status_code == 400:
+        formated_response = {
+                            'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None}
+        return formated_response
+
+    formated_response = response.json()
+    emotions = formated_response['emotionPredictions'][0]['emotion']
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    max_emotion = max(emotions, key=emotions.get)
+    #max_emotion_score = emotions[max_emotion]
+    formated_dict_emotions = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': max_emotion
+                                }
+    return formated_dict_emotions
